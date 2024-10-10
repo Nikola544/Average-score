fdaf1447f06d4f360581f4acfc09eb94146688d3
Николай <dhyeheydddjeujd>
2024-10-11 00:35:44 +0300

    11/10

---
+++ Average_score.py
@@ -0,0 +1,14 @@
+evaluations = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = ['Джонни', 'Бильбо', 'Стив', 'Гендрик', 'Аарон']
+a = [5, 3, 3, 5, 4]
+print(sum(a)/ len(a))
+b = [2, 2, 2, 3]
+print(sum(b)/ len(b))
+c = [4, 5, 5, 2]
+print(sum(c)/ len(c))
+d = [4, 4, 3]
+print(sum(d)/ len(d))
+i = [5, 5, 5, 4, 5]
+print(sum(i)/ len(i))
+print( 'Джонни:',sum(a)/ len(a) , 'Бильбо:',sum(b)/ len(b) ,
+       'Стив:',sum(c)/ len(c) , 'Гендрик:',sum(d)/ len(d) , 'Аарон:',sum(i)/ len(i) )
