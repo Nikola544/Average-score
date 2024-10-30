2a57524039517333c540767867d5fb45bdc151de
Николай <dhyeheydddjeujd>
2024-10-30 14:18:19 +0300

    30/10

--- Average_score.py
+++ Average_score.py
@@ -10,5 +10,5 @@ d = [4, 4, 3]
 print(sum(d)/ len(d))
 i = [5, 5, 5, 4, 5]
 print(sum(i)/ len(i))
-print( 'Джонни:',sum(a)/ len(a) , 'Бильбо:',sum(b)/ len(b) ,
-       'Стив:',sum(c)/ len(c) , 'Гендрик:',sum(d)/ len(d) , 'Аарон:',sum(i)/ len(i) )
+print( students [0],sum(a)/ len(a) , students [1],sum(b)/ len(b) ,
+       students [2],sum(c)/ len(c) , students [3],sum(d)/ len(d) , students [4],sum(i)/ len(i) )
