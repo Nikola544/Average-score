358c2a2ad35c566a41a359b21a102a6fafc78cc7
Николай <dhyeheydddjeujd>
2024-11-11 21:27:28 +0300

    11/11

--- Average_score.py
+++ Average_score.py
@@ -1,9 +1,5 @@
-evaluations = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-students = ['Джонни', 'Бильбо', 'Стив', 'Гендрик', 'Аарон']
-a = evaluations[0]
-b = evaluations[1]
-c = evaluations[2]
-d = evaluations[3]
-i = evaluations[4]
-print( students [0], sum(a)/ len(a) , students [1],sum(b)/ len(b) ,
-       students [2],sum(c)/ len(c) , students [3],sum(d)/ len(d) , students [4],sum(i)/ len(i) )
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Джонни', 'Бильбо', 'Стив', 'Гендрик', 'Аарон'}
+sorted_students = sorted(students)
+average_grades = {name: sum(grades[i]) / len(grades[i]) for i, name in enumerate(sorted_students)}
+print(average_grades)
